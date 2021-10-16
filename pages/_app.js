40c972c72190f25a1aa13b682879182071312254
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2021-10-16 22:15:36 +0100

    Imported CSS from my personal website

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Component {...pageProps} />
+    <footer>
+      <ul>
+        <li>
+          Created by <a href="https://www.duncanritchie.co.uk" title="Duncan Ritchie’s website">Duncan Ritchie</a>
+        </li>
+        <li>
+          Based on a tutorial from <a href="https://daily-dev-tips.com/posts/how-i-created-a-stack-guessing-tool-using-nextjs/">Chris Bongers</a>
+        </li>
+        <li>
+          <a href="https://github.com/DuncanRitchie/nextjs-tech-stack-creator" title="Repo on GitHub">See the code</a>
+        </li>
+      </ul>
+    </footer>
+  </>)
 }
 
 export default MyApp
