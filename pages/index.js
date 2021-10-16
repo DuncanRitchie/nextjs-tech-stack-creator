d7fba9df81f669420e85e5b085abbeeb29bcccec
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2021-10-16 20:26:12 +0100

    Deleted CSS

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,28 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const router = useRouter()
-  
-  const redirect = async (event) => {
-    event.preventDefault()
-    const name = event.target.name.value
-    router.push(name)
-  }
+    const router = useRouter()
 
-  return (
-    <div className={styles.container}>
+    const redirect = async (event) => {
+        event.preventDefault()
+        const name = event.target.name.value
+        router.push(name)
+    }
+
+  return (<>
       <Head>
         <title>Get a tech-stack based on your name!</title>
         <meta name="description" content="Duncan Ritchie’s take on a tutorial by daily-dev-tips.com" />
         <link rel="icon" href="https://www.duncanritchie.co.uk/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Get a tech-stack based on your name!
         </h1>
 
-        <p className={styles.description}>
+        <p>
           You’ve got a name? Let’s find your stack!
         </p>
 
@@ -33,6 +31,5 @@ export default function Home() {
           <button>Get tech-stack</button>
         </form>
       </main>
-    </div>
-  )
+  </>)
 }
