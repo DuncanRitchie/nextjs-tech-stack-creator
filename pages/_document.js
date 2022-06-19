e86cb07bef9947c8e5e6cff8999ed1ec22ff4a37
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-06-19 14:32:05 +0100

    No client-side Next.js JS in production

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  const isDev = process.env.NODE_ENV === "development";
+  return (
+    <Html>
+      <Head>
+        <link href="https://www.duncanritchie.co.uk/css/main.css" rel="stylesheet" key="main"/>
+        <link href="https://www.duncanritchie.co.uk/velut-json-generator/main.css" rel="stylesheet" key="json-generator"/>
+        {!isDev && <base href="https://duncan-tech-stacks.netlify.app/tech-stack-creator/"/>}
+      </Head>
+      <body>
+        <Main />
+        {isDev && <NextScript />}
+      </body>
+    </Html>
+  )
+}
