f13f18a6d3fe5ca1d44edd9615cb505a692ac064
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-07-03 16:54:10 +0100

    `lang` attribute

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link href="https://www.duncanritchie.co.uk/css/main.css" rel="stylesheet" key="main"/>
         <link href="https://www.duncanritchie.co.uk/css/subsites.css" rel="stylesheet" key="subsites"/>
